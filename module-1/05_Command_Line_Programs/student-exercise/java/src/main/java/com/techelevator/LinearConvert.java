04703c7dd229455c36de6503ebb7a0c840bde033
Ethan King <Eking607@gmail.com>
2020-09-21 20:56:11 -0400

    Submitting Week 1 Day 5 Exercise

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/LinearConvert.java
@@ -1,9 +1,37 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class LinearConvert {
 
 	public static void main(String[] args) {
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print("Please enter the length: ");
+	
+		String userInput = keyboard.nextLine();
+		double length = Double.parseDouble(userInput);
+		
+		System.out.print("Is the measurement in (m)eter, or (f)eet?: ");
+		
+		String userInputType = keyboard.nextLine();
+	
 
+		System.out.println(length + userInputType +  " is " + theMath(length, userInputType));
 	}
+	
+	public static double theMath(double length, String userInputType) {
+		
+		
+		if(userInputType.equals("f")) {
+			double resultMeter = (double)(( length * 0.3048));
+			return (double) resultMeter;
+			
+		} else {
+			double resultFoot = ((double)(length * 3.2808399));
+			return (double) resultFoot;
+		}
+	} 
 
 }
--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
@@ -1,9 +1,44 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class TempConvert {
 
 	public static void main(String[] args) {
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print("Please enter the temperature: ");
+	
+		String userInput = keyboard.nextLine();
+		double temp = Double.parseDouble(userInput);
+		
+		System.out.print("Is the temperature in (C)elsius or (F)ahrenheit? ");
+		
+		String userInputType = keyboard.nextLine();
+	
 
+		System.out.println(temp + userInputType +  " is " + theMath(temp, userInputType));
 	}
-
+	
+	public static double theMath(double temp, String userInputType) {
+		
+		
+		if(userInputType.equals("F")) {
+			double resultTempC = (double)(( temp - 32) / 1.8);
+			return (double) resultTempC;
+			
+		} else {
+			double resultTempF = ((double)(temp * 1.8 + 32));
+			return (double) resultTempF;
+		}
+	} 
 }
+
+
+	
+	
+
+
+
+
