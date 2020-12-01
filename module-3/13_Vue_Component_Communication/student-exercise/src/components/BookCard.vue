5f1e91df936339ec7341b28ff6209f61ab5a1b2e
Ethan King <Eking607@gmail.com>
2020-12-01 08:34:58 -0500

    Vue Component Communication

--- module-3/13_Vue_Component_Communication/student-exercise/src/components/BookCard.vue
+++ module-3/13_Vue_Component_Communication/student-exercise/src/components/BookCard.vue
@@ -1,37 +1,54 @@
 <template>
-  <div class="card">
+  <div class="card" v-bind:class="{ read: book.read }">
+    <img
+      v-if="book.isbn"
+      v-bind:src="
+        'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'
+      "
+    />
 
-    <!-- Please leave <img> commented out until directed to remove open and close comment tags in the README.
-    <img v-if="book.isbn" v-bind:src="'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'" />
-    -->
-    
+    <div class="book-title">{{ book.title }}</div>
+    <div class="book-author">{{ book.author }}</div>
+    <button class="mark-read" v-on:click.prevent="markRead" v-if="!book.read">
+      Mark Read
+    </button>
+    <button class="mark-unread" v-on:click.prevent="markRead" v-if="book.read">
+      Mark Unread
+    </button>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'book-card'
-}
+  name: "book-card",
+  props: ["book"],
+
+  methods: {
+    markRead() {
+      this.$store.commit("READ_STATUS", this.book);
+    },
+  },
+};
 </script>
 
 <style>
 .card {
-    border: 2px solid black;
-    border-radius: 10px;
-    width: 250px;
-    height: 550px;
-    margin: 20px;
+  border: 2px solid black;
+  border-radius: 10px;
+  width: 250px;
+  height: 550px;
+  margin: 20px;
 }
 
 .card.read {
-    background-color: lightgray;
+  background-color: lightgray;
 }
 
 .card .book-title {
-    font-size: 1.5rem;
+  font-size: 1.5rem;
 }
 
 .card .book-author {
-    font-size: 1rem;
+  font-size: 1rem;
 }
 </style>
